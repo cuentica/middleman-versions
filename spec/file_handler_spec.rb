a16e7e896bd9ed9b73e7fb53535ddcb2098992ea
Pedro Rivas <rmdrobs@gmail.com>
2016-11-15 10:23:29 +0100

    Test source changed

--- spec/file_handler_spec.rb
+++ spec/file_handler_spec.rb
@@ -4,10 +4,10 @@ require_relative '../lib/middleman/versions/helpers/file_handler'
 describe "FileHandler" do
   it "load_file method must return a non empty hash from json file" do
     version_hash = Middleman::Versions::FileHandler::load_file(File.join(File.dirname(__FILE__),"../fixtures/directory_with_files/v2.json"))
-    expect(version_hash).to include({"title" => "Usuario v2"})
+    expect(version_hash).to include({"swagger" => "2.0"})
   end
   it "load_file method must return a non empty hash from yml file" do
     version_hash = Middleman::Versions::FileHandler::load_file(File.join(File.dirname(__FILE__),"../fixtures/directory_with_files/v1.yml"))
-    expect(version_hash).to include({"title" => "Usuario v1"})
+    expect(version_hash).to include({"swagger" => "2.0"})
   end
 end
